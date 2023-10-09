d1475e66ae19d4d26ce601702358548378800433
Bjelogrlic84 <bjelogrlic.ivan.84@gmail.com>
2023-10-09 15:16:35 +0200

    fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
@@ -9,7 +10,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={2} groundColor='black' />
+      <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
